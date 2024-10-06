d1d3784e5ca1c1fb7ea0af8a613504f901c44c4d
Abanoub Nagy <96650563+AbanoubNagyHalem@users.noreply.github.com>
2024-10-06 11:02:59 +0300

    handle regestraion
    create database usershema register validation

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import express from "express";
-
 import cors from "cors";
-
+import connectDB from "./db/database.js";
+import userRouter from "./users/user.router.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
+
+app.use("/", userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
