c74709ef0149335b5437f723b27671fec7b80ccb
Maryam Hassan <mariam542000@gmail.com>
2024-10-09 12:55:59 +0300

    update-post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -7,8 +7,24 @@ import Post from "../db/models/post.model.js";
 
 
 // update post
+export const updatePost = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    
+    // Find the post by ID and update with the new data
+    const updatedPost = await Post.findByIdAndUpdate(id, updatedData, { new: true });
+    
+    if (!updatedPost) {
+      return res.status(404).json({ message: "Post not found" });
+    }
 
-
+    res.status(200).json({ message: "Post updated successfully", updatedPost });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err });
+  }
+};
 // delete post
 
 export const deletePost = async (req, res) => {
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -4,6 +4,6 @@ import * as postController from "../controllers/post.controller.js";
 const router = Router();
 
 router.delete("/:id", postController.deletePost);
-
+router.put("/:id", postController.updatePost);
 
 export default router;
